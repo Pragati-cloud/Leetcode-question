d93b0c34ee59b483f94c1fcf27891daa68b38921
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2025-08-24 18:00:20 +0530

    Create  Code Testcase Testcase Test Result 678. Valid Parenthesis String.java

---
+++ Code Testcase Testcase Test Result 678. Valid Parenthesis String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min =0, max=0;
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                max++;
+                min++;
+            }
+            else if (c==')'){
+                max--;
+                min--;
+            }else if(c=='*'){
+                max++;
+                min--;
+            }
+            if(max<0) return false;
+            min = Math.max(min,0);
+        }return min ==0;
+    }
+}
